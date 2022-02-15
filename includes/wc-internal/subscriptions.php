423fe18574d18031cd6ebb97a31524df9beb7d5a
Leonard Breitkopf <leonard@trunkrs.nl>
2022-02-15 17:26:28 +0100

    Fix: Fix the subscription reaction handler

--- includes/wc-internal/subscriptions.php
+++ includes/wc-internal/subscriptions.php
@@ -8,15 +8,14 @@ if (!class_exists('TRUNKRS_WC_Subscriptions')) {
       add_action('woocommerce_subscription_renewal_payment_complete', [$this, 'renewalPaymentComplete']);
     }
 
-    public function renewalPaymentComplete($subscription, $order)
+    public function renewalPaymentComplete($order)
     {
       if (!TRUNKRS_WC_Settings::getUseSubscriptionRenewals())
         return;
 
-      $initialOrder = new TRUNKRS_WC_Order($subscription, true, true);
-      if (!$initialOrder->isTrunkrsOrder) return;
+      $newOrder = new TRUNKRS_WC_Order($order, true, true);
+      if (!$newOrder->isTrunkrsOrder) return;
 
-      $newOrder = new TRUNKRS_WC_Order($order);
       $newOrder->announceShipment(true);
     }
   }
