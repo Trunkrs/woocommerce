06afcc8799aa004e3419d558235854ef378357d3
Leonard Breitkopf <leonard@trunkrs.nl>
2022-08-19 14:16:08 +0200

    Feature: Order note for transparency of moving the order date

--- includes/api.php
+++ includes/api.php
@@ -132,6 +132,7 @@ if (!class_exists('TRUNKRS_WC_Api')) {
           }
 
           if ($error->name === 'NoTimeslotFound') {
+            $order->add_order_note("The requested delivery date of [$deliveryDate] couldn't be met. Next available timeslot is used.");
             return TRUNKRS_WC_Api::announceShipment($order, $reference);
           }
         }
